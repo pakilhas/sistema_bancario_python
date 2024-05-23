fc699902bfa8c65d303dbff4c24d55fe58ed5d13
Pablo <pakilhas@gmail.com>
2024-05-23 14:24:21 -0300

    adicionado duas novas funcoes de adicionar usuario e criar conta

--- desafio_banco.py
+++ desafio_banco.py
@@ -3,15 +3,30 @@ def menu():
      
     menu = '''
     ========== MENU ==========
+    [mu] **MENU DO USUARIO**
     [d] Depositar
     [s] Sacar
     [e] Extrato
-    [m] Sair
+    
+    [q] SAIR
     ========== Bank ==========
 
 
     '''
     return input(menu)
+
+def menu2():
+    menu2 = '''
+    ========== MENU ==========
+    [c] Criar Usuario
+    [cc] Criar Conta
+    [e] Lista Conta
+    [v] Voltar
+    
+    
+    ========== Bank ==========
+    '''
+    return input(menu2)
 
 def depositar (saldo,valor,extrato, /):
     if valor > 0:
@@ -29,34 +44,85 @@ def depositar (saldo,valor,extrato, /):
 
 def sacar (*,saldo, valor, extrato, limite, limite_saques, numero_saques):
     if valor > saldo:
-        print("Saldo Indisponivel")
-        print(extrato)
+        print("Saldo Indisponivel".center(30,"#"))
+        print(f"Saldo Disponivel: R$ {saldo}")
 
     elif valor > limite:
         print("Limite maximo de saque é R$ 500 ")
 
     elif numero_saques >= limite_saques:
-        print("Limite de Saque atingido") 
+        print("Limite de Saque atingido".center(30,"#")) 
 
     elif valor > 0:
         saldo -= valor
         numero_saques = numero_saques +  1
-        print("Saque realizado com sucesso")   
+        print("Saque realizado com sucesso".center(30,"#"))   
 
     else:
         print("Apenas valores positivos são permitidos")
 
-    return saldo, extrato, numero_saques       
-
+    return saldo, extrato, numero_saques   
    
-
-
 def extrato_exibir (saldo, /, *, extrato, limite, numero_saques ):
     extrato += f"Em conta: R$ {saldo:.2f}\n Limite de Saque: R$ {limite:.2f}\n Quantidade de Saques diario: {numero_saques}\n"
     print(extrato)
     
-
     return saldo, extrato
+
+def criar_usuario(nome, cpf, usuarios, conta, agencia,/):
+    if cpf in usuarios:
+        print("Erro: CPF já cadastrado.")
+        return None
+    
+    usuarios[cpf] = nome
+    conta[0] += 1
+    print(f"Usuário {nome.upper()}\nAgencia: {agencia}\nConta: {conta}\n Cadastrado com sucesso.".title())
+    return nome, cpf, conta
+
+def criar_conta(nome, cpf, usuarios, conta, agencia,/):
+
+    if  cpf in usuarios:
+        print("Usuario já possui conta")
+        conta[0] += 1
+        print(f"Usuário {nome.upper()}\nAgencia: {agencia}\nConta: {conta}\ncadastrado com sucesso.".title())
+        print()
+
+    else:
+        print("Usuario não tem conta ")
+        menu2()    
+    
+    
+    return nome, cpf, conta
+
+def val_user():
+    usuarios = {}
+    agencia = "0001"
+    conta = [0]
+    
+    while True:
+        opcao = menu2()
+        if opcao == "c":
+            nome = str(input("Informe o Nome: "))
+            data_nas = str(input("Informe a Data de Nascimento: "))
+            cpf = str(input("Informe o CPF: "))
+            endereco = str(input("Informe o Endereço: "))
+            usuario = criar_usuario(nome, cpf, usuarios, conta, agencia)
+            if usuario:
+                print(f"Usuário {usuario[0].upper()} com CPF: {usuario[1].upper()} foi adicionado.")
+
+        if opcao == "cc":
+            nome = str(input("Informe o Nome: "))
+            cpf = str(input("Informe o CPF: "))
+            usuario = criar_conta(nome, cpf, usuarios, conta, agencia)
+            print()
+            if usuario:
+                print(f"Usuário {usuario[0].upper()} com CPF: {usuario[1].upper()} foi adicionado.")
+        
+        elif opcao == "v" :
+            menu()
+
+        elif opcao == "q":
+            break
 
 def main ():
 
@@ -71,10 +137,8 @@ def main ():
 
         if opcao == "d":
             valor = float(input("Informe o valor do depósito: "))
-
             saldo, extrato = depositar(saldo, valor, extrato)
-            
-        
+                   
         elif opcao == "s" :
             #print(saldo)
             valor = float(input("Valor que deseja sacar: "))                
@@ -82,5 +146,11 @@ def main ():
 
         elif opcao == "e" :
             saldo, extrato = extrato_exibir(saldo, extrato=extrato, limite=limite, numero_saques=numero_saques)
+        
+        elif opcao == "mu" :
+            val_user()
+        
+        elif opcao == "q" :
+            break
             
 main()
--- readme.md
+++ readme.md
@@ -10,9 +10,9 @@
 
 
 
-# Sistema Bancário em Python V1.0
+# Sistema Bancário em Python V2.0
 
-Este é um script em Python para simular operações básicas em um sistema bancário. Atualmente, o sistema suporta apenas um usuário (nome de usuário: `usuario`) e oferece funcionalidades como depósito, saque e visualização de extrato.
+Este é um script em Python para simular operações básicas em um sistema bancário. Nesta versão, foram separadas as funções de saque, depósito e extrato, e adicionadas funcionalidades para cadastrar usuários e criar contas bancárias.
 
 ## Funcionalidades
 
@@ -24,41 +24,49 @@ O script oferece as seguintes funcionalidades:
 
 3. **Extrato:** Mostra o extrato da conta, exibindo os depósitos e saques realizados, bem como o saldo atual da conta.
 
+## Novas Funcionalidades
+
+1. **Saque:** Agora recebe argumentos apenas por nome (keyword only).
+2. **Depósito:** Agora recebe argumentos apenas por posição (positional only).
+3. **Extrato:** Recebe argumentos por posição e nome (posição: saldo, nomeados: extrato).
+
 ## Uso
 
-Ao executar o script, o usuário é solicitado a inserir seu nome de usuário. Atualmente, o único nome de usuário aceito é `usuario`. Após inserir o nome de usuário, o menu é exibido com as opções disponíveis. O usuário pode escolher uma opção digitando o número correspondente.
-
+Ao executar o script, o usuário pode escolher as opções disponíveis no menu principal e no menu de usuário para realizar operações bancárias ou gerenciar usuários e contas.
 ## Exemplo de Uso
 
-```python
-digite o nome do usuario: usuario
+```python   
+    ========== MENU ==========
+    [mu] **MENU DO USUARIO**
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    
+    [q] SAIR
+    ========== Bank ==========
 
-========== MENU ==========
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[0] Sair
-========== Bank ==========
 
-1
-Deposito
-Valor a ser Depositado: 100
-deposito realizado com sucesso
-saldo atual R$ 100
-##########Depositado##########
+   ========== MENU ==========
+    [c] Criar Usuario
+    [cc] Criar Conta
+    [e] Lista Conta
+    [v] Voltar
+    
+    
+    ========== Bank ===========
 
-2
-Saldo disponivel:  R$100
-Digite o Valor do Saque: 50
-Saque realizado com sucesso
-Saldo Atual: R$ 50
-##############Saque#############
 
-3
-##########Extrato##########
-Ultimo deposito Realizado: R$100
-Valor do Saque: R$50
-Saldo Atual: R$50
-##########Extrato##########
 
+```
+# Este script é um projeto de teste e pode ser expandido para incluir recursos adicionais, como autenticação de usuário mais robusta, histórico de transações e suporte para múltiplos usuários.
+
+# Opa, 
+| Rede Social |                                            |
+|-------------|-----------------------------|
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/pablo-carvalho-93927a220/)
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=fff)](https://www.instagram.com/pablo_ddh/) [![Discord](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=fff)](https://discord.com/channels/1235957312477466717/1235957313076985858)
+[![GitHub](https://img.shields.io/badge/GitHub-%23181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/pakilhas)
+
+[![DIO](https://img.shields.io/badge/D%20I%20O-%23FFF100?style=for-the-badge&logo=digitalocean&logoColor=black)](https://web.dio.me/users/pakilhas?tab=achievements)
 
